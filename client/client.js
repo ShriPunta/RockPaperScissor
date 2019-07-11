3affbca485e157d518f6a5619f3c7506993d9bd8
Shridhar Puntambekar <shridhar@fitbliss.com>
2019-07-11 00:14:07 -0700

    Progress

--- client/client.js
+++ client/client.js
@@ -1,42 +1,92 @@
 const sock = io();
 
-sock.on('message', (data) => {
+// Custom function just to select elements
+var element = function(id){
+    return document.getElementById(id); 
+};
 
-    var messages = document.getElementById('messages');
-    console.log('--->',data);
-    if(data){
-            console.log('--data-->', data);
-            console.log('--data-->', data.length);
-            for(let i=0; i < data.length; i++){
-                console.log('--Entered->');
-                // Build out the message div
-                var msg = document.createElement('div');
-                msg.setAttribute('class', 'chat-message');
-                msg.textContent = data[i].name + ": " + data[i].message;
-                messages.appendChild(msg);
-                messages.insertBefore(msg, messages.firstChild);
+var status = element('status');
+var messages = element('messages');
+var textarea = element('textarea');
+var username = element('username');
+var clearbtn = element('clear');
 
-            }
-        }
-    
-});
+// Default status is to be blank, it will be initialised so
+var statusDefault = status.textContent;
 
-var textarea = document.querySelector("#textarea");
+// Function to set status
+var setStatus = function(s){
+    status.textContent = s; 
 
-textarea.addEventListener('keydown', function(evt){
-    if(event.which === 13 && event.shiftKey == false){
-        console.log('Enter pressed');
-        // Emit to server input
-        sock.emit('message', {
-            name : 'Def',
-            message : textarea.value
-
-        });
-        textarea.value = '';
-
-        evt.preventDefault();
+    // CLear out the status after an interval
+    if(s !== statusDefault){
+        setTimeout(function(){
+            setStatus(statusDefault);
+        }, 4000);
     }
-   
-})
+
+
+}
+if(sock !== undefined){
+    
+    sock.on('output', (data) => {
+        console.log('Connected to socket on the client side.')
+
+        var messages = document.getElementById('messages');
+        console.log('--->',data);
+        if(data){
+                console.log('--data-->', data);
+                console.log('--data-->', data.length);
+                for(let i=0; i < data.length; i++){
+                    console.log('--Entered->',data[i]);
+                    // Build out the message div
+                    var msg = document.createElement('div');
+                    msg.setAttribute('class', 'chat-message');
+                    msg.textContent = data[i].name + ": " + data[i].message;
+                    messages.appendChild(msg);
+                    messages.insertBefore(msg, messages.firstChild);
+
+                }
+            }
+        
+    });
+
+    // get the status from the server
+    sock.on('status', function(data){
+        // get message status
+        setStatus((typeof data === 'object') ? data.message : data);
+
+        // if status is clear
+        if(data.clear){
+            textarea.value = '';
+        }
+    });
+
+    textarea.addEventListener('keydown', function(evt){
+        if(event.which === 13 && event.shiftKey == false){
+            console.log('Enter pressed');
+            // Emit to server input
+            sock.emit('input', {
+                name : username.value,
+                message : textarea.value
+    
+            });
+            textarea.value = '';
+    
+            evt.preventDefault();
+        }
+    });
+
+    clearbtn.addEventListener('click', function(){
+        sock.emit('clear');
+    });
+    
+
+    sock.on('cleared', function(){
+        messages.textContent = '';
+    });
+}
+
+
 
 
--- server/server.js
+++ server/server.js
@@ -1,22 +1,113 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 5000;
 const path = require('path');
+const mongoDBConfig = require('../config/keys');
 server.listen(PORT, () => console.log(`Server started on ${PORT}`));
+const clientPath =`${__dirname}/../client`;
+console.log(`Serving static from ${clientPath}`);
 
 //Init middleware
 app.use(express.json({extended: true}));
+
+// Static files
+app.use(express.static(clientPath));
+
+
+// DB Config
+
+MongoClient
+  .connect(
+    mongoDBConfig.mongoConnectURI,
+    { useNewUrlParser: true },
+    function(err, mongoCallback){
+        if(err){
+            console.log('Entered error');
+            throw err;
+        }
+
+        console.log('Mongo connected Successfully -->');
+        
+
+        const db = mongoCallback.db(mongoDBConfig.mongoDBName);
+        
+        io.on('connection', (currConnSock) => {
+          let chat = db.collection('chats');
+
+          console.log('Someone connected');
+
+          // Write a function to send status to the newly connected client
+          var sendStatus = (s) => {
+            currConnSock.emit('status',s);
+          };
+          //currConnSock.emit('message', 'Hi you are connected');
+
+
+          // Get chats from Moongo database
+          chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+              throw err;
+            }
+
+            // Emit the messages
+            currConnSock.emit('output', res);
+          });
+
+          // Handle input events if someone types something from the client, we will handle it here
+          io.on('input', function(data){
+            let name = data.name;
+            let msg = data.msg;
+
+
+            // Null check for name and message
+            // Also handle this on the client side
+            if(name == '' || msg == ''){
+              sendStatus('Please enter a name and meesgae');
+            } else{
+              // Insert the sent message to MongoDB
+              chat.insert({name: name, message:message}, function(){
+
+                // Whatever the client sends, he should also see it on his screen, so we emit it back
+                currConnSock.emit('output', [data]);
+
+                //Send status obj
+                sendStatus({
+                  message : 'Message sent',
+                  clear : true
+                });
+              });
+            }
+          });
+
+          // Handle clear
+          io.on('clear', function(data){
+            // Remove all chats from the collection in Mongo
+            chat.remove({}, function(){
+              // EMit cleared
+              currConnSock.emit('cleared');
+            });
+          });
+
+        });
+
+
+
+      }
+  );
+
 
 var generateNewId = () => { return (new Date).getTime().toString('16') };
 
 
 app.get('/', function (req, res) {
     //res.sendFile(path.resolve(`${__dirname}/../client/index.html`));
+    res.send('index.html');
     let newId = generateNewId();
     console.log('-newId-->',newId);
-    res.redirect(`/game/${newId}`);
+    //res.redirect(`/game/${newId}`);
 
   });
 
